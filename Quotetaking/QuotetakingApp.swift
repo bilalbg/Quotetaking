d822c9ea7776b4cc41c30d4ba6637cc02c41e91c
bilalbg <bilal.baig316@gmail.com>
2023-12-16 15:59:45 -0500

    Initial Commit

---
+++ Quotetaking/QuotetakingApp.swift
@@ -0,0 +1,20 @@
+//
+//  QuotetakingApp.swift
+//  Quotetaking
+//
+//  Created by Bilal Baig on 2023-12-16.
+//
+
+import SwiftUI
+
+@main
+struct QuotetakingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ QuotetakingUITests/QuotetakingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  QuotetakingUITestsLaunchTests.swift
+//  QuotetakingUITests
+//
+//  Created by Bilal Baig on 2023-12-16.
+//
+
+import XCTest
+
+final class QuotetakingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
